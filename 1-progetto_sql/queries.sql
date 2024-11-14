0fd01e0482fc98e7e1889d8eb0010e7895654161
tuttigiuperterraa <giulia.cosenza1303@gmail.com>
2024-11-14 10:05:24 +0100

    queries a posto in merito a co2 5 maggiori food inquinanti per i 5 paesi con piu produzione di co2

--- 1-progetto_sql/queries.sql
+++ 1-progetto_sql/queries.sql
@@ -90,11 +90,12 @@ explained_perception_of_corruption float,
 dystopia_plus_residual float
 );
 
-DROP TABLE food;
+
 --- Tabella prodotti alimentari
 CREATE TABLE food(
 food_domain varchar(255),
 area varchar(255),
+food_element varchar(255),
 item varchar(255),
 years int,
 food_value float
@@ -106,7 +107,7 @@ continent varchar(40),
 country varchar(40) unique PRIMARY KEY 
 );
 
-DROP TABLE food_co2;
+
 CREATE TABLE food_co2(
 prodotto varchar(20) PRIMARY KEY unique,
 code varchar(20),
@@ -115,22 +116,402 @@ kgco2_per_kgprodotto float
 );
 */
 
+
 --- Visualizziamo i 5 paesi con maggior produzione di co2
-SELECT country_name, population, density, co2
+SELECT country_name, population, density, co2_tons
 FROM global_var
-WHERE co2 IS NOT NULL
-ORDER BY co2 DESC
+WHERE co2_tons IS NOT NULL
+ORDER BY co2_tons DESC
 LIMIT 5;	-- China
 			-- USA
 			-- India
 			-- Russia
 			-- Japan
 
---- Visualizziamo gli alimenti che inquinano di più
 
---- Visualizza i paesi che producono maggiormente i 3 principali alimenti inquinanti nei 5 paesi
+--- Visualizza i 5 paesi con la popolazione più alta
+SELECT country_name, population, density, co2
+FROM global_var
+WHERE population IS NOT NULL
+ORDER BY population DESC
+LIMIT 10;
 
---- Percentuale di co2 prodotta dai 3 principali alimenti inquinanti nei 5 paesi
+
+--- Visualizziamo i 5 alimenti che inquinano di più
+SELECT prodotto, kgco2_per_kgprodotto AS co2
+FROM food_co2
+ORDER BY co2 DESC
+LIMIT 5;
+					-- Beef
+				    -- Dark chocolate
+					-- Lamb & Mutton
+					-- Beef dairy
+					-- Coffee
+
+
+--- CHINA
+WITH china_food_co2
+AS(
+	--%co2 da beef
+	SELECT area, 'Beef (beef herd)' AS item, SUM(food_value) AS tons
+	FROM food
+	WHERE 
+	    area LIKE 'China' 
+	    AND item ~* 'buffalo'  --rende case-insensitive la ricerca
+	    AND item !~* 'milk'
+		AND years = 2022
+	GROUP BY area
+
+	UNION ALL
+	
+	--%co2 da chocolate
+	SELECT area, 'Dark Chocolate' AS item, SUM(food_value) AS tons
+	FROM food
+	WHERE 
+	    area LIKE 'China' 
+	    AND item ~* 'cocoa'  --rende case-insensitive la ricerca
+		AND years = 2022
+	GROUP BY area
+
+	UNION ALL
+	
+	--%co2 da lamb
+	SELECT area, 'Lamb & Mutton' AS item, SUM(food_value) AS tons
+	FROM food
+	WHERE 
+	    area LIKE 'China' 
+	    AND item ~* 'lamb'  --rende case-insensitive la ricerca
+		AND years = 2022
+	GROUP BY area
+
+	UNION ALL
+	
+	--%co2 da beef milk
+	SELECT area, 'Beef (dairy herd)' AS item, SUM(food_value) AS tons
+	FROM food
+	WHERE 
+	    area LIKE 'China' 
+	    AND item ~* 'buffalo'  --rende case-insensitive la ricerca
+	    AND item ~* 'milk'
+		AND years = 2022
+	GROUP BY area
+
+	UNION ALL
+	
+	--%co2 da coffee
+	SELECT area, 'Coffee' AS item, SUM(food_value) AS tons
+	FROM food
+	WHERE 
+	    area LIKE 'China' 
+	    AND item ~* 'coffee'  --rende case-insensitive la ricerca
+		AND years = 2022
+	GROUP BY area
+)
+SELECT cfc.area, cfc.item, tons, co2_per_kg, (tons * 1000 * co2_per_kg) / 1000 AS co2_tot_tons
+FROM china_food_co2 AS cfc
+INNER JOIN(
+	SELECT prodotto, kgco2_per_kgprodotto AS co2_per_kg
+	FROM food_co2
+	ORDER BY co2_per_kg DESC
+	LIMIT 5
+) fc
+ON cfc.item = fc.prodotto;
+
+
+--- USA
+WITH usa_food_co2
+AS(
+	--%co2 da beef
+	SELECT area, 'Beef (beef herd)' AS item, SUM(food_value) AS tons
+	FROM food
+	WHERE 
+	    area LIKE 'United States of America' 
+	    AND item ~* 'buffalo'  --rende case-insensitive la ricerca
+	    AND item !~* 'milk'
+		AND years = 2022
+	GROUP BY area
+
+	UNION ALL
+	
+	--%co2 da chocolate
+	SELECT area, 'Dark Chocolate' AS item, SUM(food_value) AS tons
+	FROM food
+	WHERE 
+	    area LIKE 'United States of America' 
+	    AND item ~* 'cocoa'  --rende case-insensitive la ricerca
+		AND years = 2022
+	GROUP BY area
+
+	UNION ALL
+	
+	--%co2 da lamb
+	SELECT area, 'Lamb & Mutton' AS item, SUM(food_value) AS tons
+	FROM food
+	WHERE 
+	    area LIKE 'United States of America' 
+	    AND item ~* 'lamb'  --rende case-insensitive la ricerca
+		AND years = 2022
+	GROUP BY area
+
+	UNION ALL
+	
+	--%co2 da beef milk
+	SELECT area, 'Beef (dairy herd)' AS item, SUM(food_value) AS tons
+	FROM food
+	WHERE 
+	    area LIKE 'United States of America' 
+	    AND item ~* 'buffalo'  --rende case-insensitive la ricerca
+	    AND item ~* 'milk'
+		AND years = 2022
+	GROUP BY area
+
+	UNION ALL
+	
+	--%co2 da coffee
+	SELECT area, 'Coffee' AS item, SUM(food_value) AS tons
+	FROM food
+	WHERE 
+	    area LIKE 'United States of America' 
+	    AND item ~* 'coffee'  --rende case-insensitive la ricerca
+		AND years = 2022
+	GROUP BY area
+)
+SELECT ufc.area, ufc.item, ufc.tons, fc.co2_per_kg, (ufc.tons * 1000 * fc.co2_per_kg) / 1000 AS co2_tot_tons
+FROM usa_food_co2 AS ufc
+INNER JOIN(
+	SELECT prodotto, kgco2_per_kgprodotto AS co2_per_kg
+	FROM food_co2
+	ORDER BY co2_per_kg DESC
+	LIMIT 5
+) fc
+ON ufc.item = fc.prodotto;
+
+
+--- INDIA
+WITH india_food_co2
+AS(
+	--%co2 da beef
+	SELECT area, 'Beef (beef herd)' AS item, SUM(food_value) AS tons
+	FROM food
+	WHERE 
+	    area LIKE 'India' 
+	    AND item ~* 'buffalo'  --rende case-insensitive la ricerca
+	    AND item !~* 'milk'
+		AND years = 2022
+	GROUP BY area
+
+	UNION ALL
+	
+	--%co2 da chocolate
+	SELECT area, 'Dark Chocolate' AS item, SUM(food_value) AS tons
+	FROM food
+	WHERE 
+	    area LIKE 'India' 
+	    AND item ~* 'cocoa'  --rende case-insensitive la ricerca
+		AND years = 2022
+	GROUP BY area
+
+	UNION ALL
+	
+	--%co2 da lamb
+	SELECT area, 'Lamb & Mutton' AS item, SUM(food_value) AS tons
+	FROM food
+	WHERE 
+	    area LIKE 'India' 
+	    AND item ~* 'lamb'  --rende case-insensitive la ricerca
+		AND years = 2022
+	GROUP BY area
+
+	UNION ALL
+	
+	--%co2 da beef milk
+	SELECT area, 'Beef (dairy herd)' AS item, SUM(food_value) AS tons
+	FROM food
+	WHERE 
+	    area LIKE 'India' 
+	    AND item ~* 'buffalo'  --rende case-insensitive la ricerca
+	    AND item ~* 'milk'
+		AND years = 2022
+	GROUP BY area
+
+	UNION ALL
+	
+	--%co2 da coffee
+	SELECT area, 'Coffee' AS item, SUM(food_value) AS tons
+	FROM food
+	WHERE 
+	    area LIKE 'India' 
+	    AND item ~* 'coffee'  --rende case-insensitive la ricerca
+		AND years = 2022
+	GROUP BY area
+)
+SELECT ifc.area, ifc.item, ifc.tons, fc.co2_per_kg, (ifc.tons * 1000 * fc.co2_per_kg) / 1000 AS co2_tot_tons
+FROM india_food_co2 AS ifc
+INNER JOIN(
+	SELECT prodotto, kgco2_per_kgprodotto AS co2_per_kg
+	FROM food_co2
+	ORDER BY co2_per_kg DESC
+	LIMIT 5
+) fc
+ON ifc.item = fc.prodotto;
+
+
+--- RUSSIA
+WITH russia_food_co2
+AS(
+	--%co2 da beef
+	SELECT area, 'Beef (beef herd)' AS item, SUM(food_value) AS tons
+	FROM food
+	WHERE 
+	    area LIKE 'Russian Federation' 
+	    AND item ~* 'buffalo'  --rende case-insensitive la ricerca
+	    AND item !~* 'milk'
+		AND years = 2022
+	GROUP BY area
+
+	UNION ALL
+	
+	--%co2 da chocolate
+	SELECT area, 'Dark Chocolate' AS item, SUM(food_value) AS tons
+	FROM food
+	WHERE 
+	    area LIKE 'Russian Federation' 
+	    AND item ~* 'cocoa'  --rende case-insensitive la ricerca
+		AND years = 2022
+	GROUP BY area
+
+	UNION ALL
+	
+	--%co2 da lamb
+	SELECT area, 'Lamb & Mutton' AS item, SUM(food_value) AS tons
+	FROM food
+	WHERE 
+	    area LIKE 'Russian Federation' 
+	    AND item ~* 'lamb'  --rende case-insensitive la ricerca
+		AND years = 2022
+	GROUP BY area
+
+	UNION ALL
+	
+	--%co2 da beef milk
+	SELECT area, 'Beef (dairy herd)' AS item, SUM(food_value) AS tons
+	FROM food
+	WHERE 
+	    area LIKE 'Russian Federation' 
+	    AND item ~* 'buffalo'  --rende case-insensitive la ricerca
+	    AND item ~* 'milk'
+		AND years = 2022
+	GROUP BY area
+
+	UNION ALL
+	
+	--%co2 da coffee
+	SELECT area, 'Coffee' AS item, SUM(food_value) AS tons
+	FROM food
+	WHERE 
+	    area LIKE 'Russian Federation' 
+	    AND item ~* 'coffee'  --rende case-insensitive la ricerca
+		AND years = 2022
+	GROUP BY area
+)
+SELECT rfc.area, rfc.item, rfc.tons, fc.co2_per_kg, (rfc.tons * 1000 * fc.co2_per_kg) / 1000 AS co2_tot_tons
+FROM russia_food_co2 AS rfc
+INNER JOIN(
+	SELECT prodotto, kgco2_per_kgprodotto AS co2_per_kg
+	FROM food_co2
+	ORDER BY co2_per_kg DESC
+	LIMIT 5
+) fc
+ON rfc.item = fc.prodotto;
+
+
+--- JAPAN
+WITH japan_food_co2
+AS(
+	--%co2 da beef
+	SELECT area, 'Beef (beef herd)' AS item, SUM(food_value) AS tons
+	FROM food
+	WHERE 
+	    area LIKE 'Japan' 
+	    AND item ~* 'buffalo'  --rende case-insensitive la ricerca
+	    AND item !~* 'milk'
+		AND years = 2022
+	GROUP BY area
+
+	UNION ALL
+	
+	--%co2 da chocolate
+	SELECT area, 'Dark Chocolate' AS item, SUM(food_value) AS tons
+	FROM food
+	WHERE 
+	    area LIKE 'Japan' 
+	    AND item ~* 'cocoa'  --rende case-insensitive la ricerca
+		AND years = 2022
+	GROUP BY area
+
+	UNION ALL
+	
+	--%co2 da lamb
+	SELECT area, 'Lamb & Mutton' AS item, SUM(food_value) AS tons
+	FROM food
+	WHERE 
+	    area LIKE 'Japan' 
+	    AND item ~* 'lamb'  --rende case-insensitive la ricerca
+		AND years = 2022
+	GROUP BY area
+
+	UNION ALL
+	
+	--%co2 da beef milk
+	SELECT area, 'Beef (dairy herd)' AS item, SUM(food_value) AS tons
+	FROM food
+	WHERE 
+	    area LIKE 'Japan' 
+	    AND item ~* 'buffalo'  --rende case-insensitive la ricerca
+	    AND item ~* 'milk'
+		AND years = 2022
+	GROUP BY area
+
+	UNION ALL
+	
+	--%co2 da coffee
+	SELECT area, 'Coffee' AS item, SUM(food_value) AS tons
+	FROM food
+	WHERE 
+	    area LIKE 'Japan' 
+	    AND item ~* 'coffee'  --rende case-insensitive la ricerca
+		AND years = 2022
+	GROUP BY area
+)
+SELECT jfc.area, jfc.item, jfc.tons, fc.co2_per_kg, (jfc.tons * 1000 * fc.co2_per_kg) / 1000 AS co2_tot_tons
+FROM japan_food_co2 AS jfc
+INNER JOIN(
+	SELECT prodotto, kgco2_per_kgprodotto AS co2_per_kg
+	FROM food_co2
+	ORDER BY co2_per_kg DESC
+	LIMIT 5
+) fc
+ON jfc.item = fc.prodotto;
+
+
+
+--- Percentuale di co2 prodotta dal principale alimenti inquinante nei 5 paesi
+
+-- CHINA
+-- principale prodotto
+SELECT * 
+FROM food
+WHERE 
+    area LIKE 'China' 
+	AND years = 2022
+ORDER BY food_value DESC
+LIMIT 1;
+-- %co2 principale prodotto
+SELECT * 
+FROM food_co2
+WHERE prodotto LIKE 'Egg%';
+
+
 
 --- Andamento produzione di manzo nei 5 paesi nel tempo 
 --https://www.fao.org/faostat/en/#data/QCL
