73267a4b3797b1e59533a4d3e2048dfbe38d8273
tuttigiuperterraa <giulia.cosenza1303@gmail.com>
2024-11-15 16:57:45 +0100

    nella costruzione delle tabelle ho assegnato il corretto valore di char alle stringhe

--- 1-progetto_sql/queries.-3foods.sql
+++ 1-progetto_sql/queries.-3foods.sql
@@ -1,46 +1,45 @@
 -- Costruisco il database
 CREATE DATABASE progetto_sqldb;
 
-
 -- Costruisco le tabelle 
 
 --- Tabella global_var - source : https://www.kaggle.com/datasets/nelgiriyewithana/countries-of-the-world-2023
 CREATE TABLE global_var(
-country_name varchar(255) not null unique PRIMARY KEY,
+country_name varchar(40) not null unique PRIMARY KEY,
 density int,
-abbreviation varchar(5),
-agricultural_land_p varchar(255),
+abbreviation char(2),
+agricultural_land_p varchar(10),
 land_area float,
 armed_forces_size float,
 birth_rate float,
 calling_code int,
-capital varchar(255),
+capital varchar(30),
 co2 bigint,
 cpi float,
-cpi_change_p varchar(255),
-currency varchar(255),
+cpi_change_p varchar(10),
+currency char(3),
 fertility_rate float,
-forested_area_p varchar(255),
+forested_area_p varchar(10),
 gasoline_price varchar(7),
 gdp varchar(20),
-gross_primary_education_enrollment_p varchar(255),
-gross_tertiary_education_enrollment_p varchar(255),
+gross_primary_education_enrollment_p varchar(10),
+gross_tertiary_education_enrollment_p varchar(10),
 infant_mortality float,
-largest_city varchar(255),
+largest_city varchar(30),
 life_expectancy float,
 maternal_mortality int,
-minimum_wage varchar(255),
-official_language varchar(255),
-out_of_pocket_health_expenditure_p varchar(255),
+minimum_wage varchar(6),
+official_language varchar(30),
+out_of_pocket_health_expenditure_p varchar(10),
 physicians float,
 population bigint,
-labor_force_participation_p varchar(255),
-tax_revenue_p varchar(255),
-total_tax_rate_p varchar(255),
-unemployment_rate_p varchar(255),
+labor_force_participation_p varchar(10),
+tax_revenue_p varchar(10),
+total_tax_rate_p varchar(10),
+unemployment_rate_p varchar(10),
 urban_population int,
-latitude varchar(255),
-longitude varchar(255)
+latitude varchar(15),
+longitude varchar(15)
 );
 
 UPDATE global_var
@@ -49,8 +48,8 @@ WHERE country_name LIKE 'United States';
 
 --- Tabella andamento co2 - source : https://ourworldindata.org/co2-emissions
 CREATE TABLE co2_over_years(
-country varchar(255),
-country_code varchar(255),
+country varchar(40),
+country_code varchar(10),
 years int,
 co2_in_tons float,
 PRIMARY KEY (country, years)
@@ -62,10 +61,10 @@ WHERE country LIKE 'United States';
 
 --- Tabella prodotti alimentari - source: https://www.fao.org/faostat/en/#data/QCL
 CREATE TABLE food(
-food_domain varchar(255),
-area varchar(255),
-food_element varchar(255),
-item varchar(255),
+food_domain char(28),
+area varchar(60),
+food_element char(10),
+item varchar(150),
 years int,
 food_value float
 );
